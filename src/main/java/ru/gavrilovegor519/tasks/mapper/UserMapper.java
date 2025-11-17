3a63751e616cee58534b8a22f12dfea85d7dcda2
Egor Gavrilov <gavrilovegor519-2@yandex.ru>
2025-11-17 23:22:41 +0300

    Fix login to app.

--- src/main/java/ru/gavrilovegor519/tasks/mapper/UserMapper.java
+++ src/main/java/ru/gavrilovegor519/tasks/mapper/UserMapper.java
@@ -1,21 +1,13 @@
 package ru.gavrilovegor519.tasks.mapper;
 
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 import ru.gavrilovegor519.tasks.dto.input.users.LoginDto;
 import ru.gavrilovegor519.tasks.dto.input.users.RegDto;
 import ru.gavrilovegor519.tasks.entity.User;
-import ru.gavrilovegor519.tasks.mapper.util.EncodedMapping;
-import ru.gavrilovegor519.tasks.mapper.util.PasswordEncoderMapper;
 
-@Mapper(componentModel = "spring",
-        uses = PasswordEncoderMapper.class,
-        unmappedTargetPolicy = ReportingPolicy.IGNORE)
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface UserMapper {
-    @Mapping(source = "password", target = "password")
     User map(LoginDto dto);
-
-    @Mapping(source = "password", target = "password", qualifiedBy = EncodedMapping.class)
     User map(RegDto dto);
 }
