c453f614171eba3d69de86b3f440289878cfbe3a
Egor Gavrilov <gavrilovegor519-2@yandex.ru>
2026-01-13 19:52:06 +0300

    Fix MVC layer

--- src/main/java/ru/gavrilovegor519/tasks/TasksApplication.java
+++ src/main/java/ru/gavrilovegor519/tasks/TasksApplication.java
@@ -2,10 +2,13 @@ package ru.gavrilovegor519.tasks;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableTransactionManagement
+@EnableSpringDataWebSupport(pageSerializationMode = PageSerializationMode.VIA_DTO)
 public class TasksApplication {
 
 	public static void main(String[] args) {
--- src/main/java/ru/gavrilovegor519/tasks/controller/CustomExceptionResolver.java
+++ src/main/java/ru/gavrilovegor519/tasks/controller/CustomExceptionResolver.java
@@ -1,10 +1,11 @@
 package ru.gavrilovegor519.tasks.controller;
 
-import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import lombok.extern.log4j.Log4j2;
 import ru.gavrilovegor519.tasks.dto.output.Response;
 import ru.gavrilovegor519.tasks.exception.CommentNotFoundException;
 import ru.gavrilovegor519.tasks.exception.ForbiddenChangesException;
@@ -28,13 +29,6 @@ public class CustomExceptionResolver {
         log.error(e.getMessage());
         Response<Object> response = new Response<>(e.getMessage(), false);
         return new ResponseEntity<>(response, HttpStatus.FORBIDDEN);
-    }
-
-    @ExceptionHandler(Throwable.class)
-    public ResponseEntity<Response<Object>> otherHandler(Throwable e) {
-        log.error("Unexpected error occurred", e);
-        Response<Object> response = new Response<>("An unexpected error occurred", false);
-        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
 }
