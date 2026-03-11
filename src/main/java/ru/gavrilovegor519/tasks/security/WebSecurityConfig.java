90efd81a1887e6b6c3114a81646296fc1520895d
Egor Gavrilov <gavrilovegor519-2@yandex.ru>
2026-03-11 13:38:06 +0300

    Finalize migrating to Keycloak.

--- src/main/java/ru/gavrilovegor519/tasks/security/WebSecurityConfig.java
+++ src/main/java/ru/gavrilovegor519/tasks/security/WebSecurityConfig.java
@@ -2,12 +2,13 @@ package ru.gavrilovegor519.tasks.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -23,7 +24,22 @@ public class WebSecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .anyRequest().authenticated())
-                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(jwt -> jwt
+                                .jwtAuthenticationConverter(keycloakJwtConverter())
+                        )
+                );
         return http.build();
+    }
+
+    private JwtAuthenticationConverter keycloakJwtConverter() {
+        JwtGrantedAuthoritiesConverter jwtGrantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+        jwtGrantedAuthoritiesConverter.setAuthoritiesClaimName("scope");
+
+        KeycloakRoleConverter keycloakRoleConverter = new KeycloakRoleConverter();
+
+        JwtAuthenticationConverter jwtConverter = new JwtAuthenticationConverter();
+        jwtConverter.setJwtGrantedAuthoritiesConverter(keycloakRoleConverter);
+        return jwtConverter;
     }
 }
